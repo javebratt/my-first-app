0e5882663956f4a27228f9e8fa3d96e64df02a47
Jorge Vergara <javebratt@gmail.com>
2021-03-13 14:48:01 -0500

    Technical SEO

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,11 @@ import { AngularFirestore } from '@angular/fire/firestore';
 })
 export class HomePage {
   movies;
-  constructor(private firestore: AngularFirestore) {
+  constructor(
+    private firestore: AngularFirestore,
+    private titleService: Title,
+    private metaTags: Meta
+  ) {
     this.firestore
       .collection('movies')
       .valueChanges({ idField: 'movieId' })
@@ -16,5 +21,10 @@ export class HomePage {
         this.movies = movies;
         console.log(movies);
       });
+    this.titleService.setTitle(`Movie List`);
+    this.metaTags.updateTag({
+      name: 'description',
+      content: 'This is the description of my website',
+    });
   }
 }
